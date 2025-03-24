0c982873d11d38c7b8d4848d91414ddf94010266
Michelle Alejandra Díaz Figueroa <midifi26@gmail.com>
2025-03-24 09:16:15 +0100

    Ya funciona el codigo, excepto el boton de borrar un contacto

--- script.js
+++ script.js
@@ -7,22 +7,20 @@ document.querySelector('form').addEventListener('submit', function(event) {
         mensaje:document.getElementById('mensaje').value,
         url:document.getElementById('url').value
     };
-    // Agregar los nuevos datos al arreglo
+    
     formulario.push(datos);
 
-    // Guardar el arreglo actualizado nuevamente en localStorage
+    
     localStorage.setItem('formulario', JSON.stringify(formulario));
     });
 
     
-    let eliminarLS = document.getElementById('borrarLS').addEventListener('submit',function(){
-     
-     return localStorage.clear();
-    });
+let eliminarLS = document.getElementById('borrarLS').addEventListener('submit',function(){
+    return localStorage.clear();
+ });
 
-     document.getElementById('url').addEventListener('click',function(){
-             
-            return localStorage.removeItem('url');
-         })
+ document.getElementById('url').addEventListener('click',function(){
+     return localStorage.removeItem('url');
+ })
 
     
